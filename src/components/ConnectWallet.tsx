8b4dd34afd9362d87894a0c904f19a4092d0397d
edrian78 <edrian@wzard.co>
2025-06-07 21:10:30 +0700

    add useRef

--- src/components/ConnectWallet.tsx
+++ src/components/ConnectWallet.tsx
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react';
+import React, { useState,useRef } from 'react';
 import { Wallet, CheckCircle, ExternalLink, QrCode, X } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
