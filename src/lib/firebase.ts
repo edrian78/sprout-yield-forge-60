39655524d44a8097b7c6b189450f8bf45e049620
edrian78 <edrian@wzard.co>
2025-06-07 20:32:43 +0700

    add firebase config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,14 @@ import { getFunctions } from 'firebase/functions';
 
 // Firebase configuration - you can update these values
 const firebaseConfig = {
-  apiKey: "your-api-key",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "your-app-id"
+  apiKey: "AIzaSyC3L4ZH8WE0W-ewwC2ppWb35DziwVa3zJE",
+  authDomain: "wzard-ecb8c.firebaseapp.com",
+  databaseURL: "https://wzard-ecb8c-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "wzard-ecb8c",
+  storageBucket: "wzard-ecb8c.firebasestorage.app",
+  messagingSenderId: "866307070417",
+  appId: "1:866307070417:web:60396f70db625b5251a8f0",
+  measurementId: "G-VLMR424WWM"
 };
 
 // Initialize Firebase
